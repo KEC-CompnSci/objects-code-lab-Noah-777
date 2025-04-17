39902ee8a63e12b25e45e91c2cff84d993089e6c
Noah-777 <25agustn@retsdstudent.ca>
2025-04-17 16:42:28 +0000

    Implemented Question class

--- src/main/java/Question.java
+++ src/main/java/Question.java
@@ -2,5 +2,29 @@ import java.util.Scanner;
 
 
 public class Question {
-  
+  public String question;
+  public int totalMarks;
+  public String answer;
+  public String studentAnswer ="";
+
+  public Question(String question, int totalMarks, String answer){
+    this.question = question;
+    this.totalMarks = totalMarks;
+    this.answer = answer;
+}
+
+public void AskQuestion(Scanner scanner){
+    System.out.println(question + " ("+totalMarks+" marks)");
+    System.out.println("Your answer: ");
+    studentAnswer = scanner.nextLine();
+}
+public int CheckAnswer(){
+    if (studentAnswer.equals(answer)){
+        System.out.println("Correct");
+        return totalMarks;
+    }
+    System.out.println("Incorrect");
+    return 0;
+    
+}
 }
